87aa7a05b11c06ccf77355149faf9be7dd7e311b
mpaulweeks <mpaulweeks@gmail.com>
2017-03-26 11:36:01 -0400

    draft css

--- quiz-app/src/MTG.js
+++ quiz-app/src/MTG.js
@@ -1,20 +1,20 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import './App.css';
+import './MTG.css';
 
 class Card extends Component {
   render() {
     return (
-      <div className="card" onClick={() => this.props.onClick()}>
-        <div className="name">
+      <div className="Card" onClick={() => this.props.onClick()}>
+        <div className="Card-name">
           {this.props.data.name}
         </div>
-        <div className="type">
+        <div className="Card-type">
           {this.props.data.type}
         </div>
-        <div className="text">
+        <div className="Card-text">
           {this.props.data.text.split("\n").map(function(line, index) {
-            return <div className="line" key={index}>{line}</div>
+            return <div className="Card-text-line" key={index}>{line}</div>
           })}
         </div>
       </div>
@@ -23,12 +23,16 @@ class Card extends Component {
 }
 
 function Manager(props) {
-  const card = props.data['Masticore'];
-  console.log(card);
+  const card1 = props.data['Masticore'];
+  const card2 = props.data['Fog'];
+  console.log(card1);
   return (
-    <div>
-      Hello
-      <Card data={card} />
+    <div className="Manager">
+      MTG QUIZ
+      <div className="Container">
+        <Card data={card1} />
+        <Card data={card2} />
+      </div>
     </div>
   )
 }
