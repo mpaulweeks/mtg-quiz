d7e992dbfe5731a23334e20125b370ef4fe1fe05
mpaulweeks <mpaulweeks@gmail.com>
2017-03-26 18:50:31 -0400

    dataArray is working, render reloads Manager but not Card

--- quiz-app/src/MTG.js
+++ quiz-app/src/MTG.js
@@ -2,69 +2,92 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './MTG.css';
 
-var MTG = {};
+const MTG = {};
 
 class Card extends Component {
   constructor(props) {
     super(props);
-    var cData = props.cData;
-    console.log(cData);
+    const cData = props.cData;
     cData.body = cData.text || "";
+    cData.prettyCost = (cData.manaCost || '0').replace(/\{|\}/g, '')
     cData.pt = "";
     if (cData.hasOwnProperty('power')){
       cData.pt = cData.power + '/' + cData.toughness;
     }
-    cData.cost = MTG.get.cardCost(cData);
-    cData.onClick = function(){};
-    this.cData = cData;
+    console.log(cData);
+    this.state = {
+      cData: cData,
+      callback: props.callback,
+    }
   }
   render() {
     return (
-      <div className="Card" onClick={() => this.cData.onClick()}>
+      <div className="Card" onClick={this.state.callback}>
         <div className="Card-name">
-          {this.cData.name}
+          {this.state.cData.name}
         </div>
         <div className="Card-type">
-          {this.cData.type}
+          {this.state.cData.type}
         </div>
         <div className="Card-text">
-          {this.cData.body.split("\n").map(function(line, index) {
+          {this.state.cData.body.split("\n").map(function(line, index) {
             return <div className="Card-text-line" key={index}>{line}</div>
           })}
         </div>
+        <div className="Card-cost">
+          {this.state.cData.prettyCost}
+        </div>
         <div className="Card-pt">
-          {this.cData.pt}
+          {this.state.cData.pt}
         </div>
       </div>
     )
   }
 }
 
-function Manager(props) {
-  const card1 = props.data['Cryptic Command'];
-  // const card2 = props.data['Fog'];
-  // const card1 = props.data['Glory Seeker'];
-  // const card2 = props.data['Soulless One'];
-  // const card2 = props.data['Monastery Mentor'];
-  const card2 = props.data['Kozilek, the Great Distortion'];
-  return (
-    <div className="Manager">
-      <div className="Title">
-        MTG QUIZ
+class Manager extends Component {
+  constructor(){
+    super();
+    this.state = {
+      cards: MTG.randomPair(),
+    };
+    this.reload = this.reload.bind(this);
+  }
+  reload(e){
+    console.log('reloading');
+    this.setState({
+      cards: MTG.randomPair()
+    }, function(){
+      console.log('done rendering');
+      console.log(this.state.cards);
+    });
+  }
+  render() {
+    return (
+      <div className="Manager">
+        <div className="Title">
+          MTG QUIZ
+        </div>
+        <div>
+          {this.state.cards[0].name}
+        </div>
+        <div className="Container">
+          <Card key='1' cData={this.state.cards[0]} callback={this.reload} />
+          <Card key='2' cData={this.state.cards[1]} callback={this.reload} />
+        </div>
+        <div className="Options">
+          Question
+        </div>
       </div>
-      <div className="Container">
-        <Card cData={card1} />
-        <Card cData={card2} />
-      </div>
-      <div className="Options">
-        Question
-      </div>
-    </div>
-  )
+    )
+  }
 }
 
 MTG.get = {};
 MTG.get.cardCost = function(cData){
+  if (cData.graphCost){
+    return cData.graphCost;
+  }
   if (!cData.manaCost || cData.manaCost.indexOf('/') > -1){
     return null;
   }
@@ -97,10 +120,11 @@ MTG.get.cardCost = function(cData){
       functionalCost += 2 * count;
     }
   });
-  return {
+  cData.graphCost = {
     bins: bins,
     functionalCost: functionalCost,
   };
+  return cData.graphCost;
 }
 MTG.get.cardDistance = function(cData1, cData2){
   const cost1 = MTG.get.cardCost(cData1);
@@ -110,20 +134,21 @@ MTG.get.cardDistance = function(cData1, cData2){
   }
   return null;
 }
-MTG.drawGraph = function(data){
+MTG.drawGraph = function(dataArray){
   var byColors = {};
-  Object.keys(data).forEach(function(cKey){
-    var cData = data[cKey];
-    var colorKey = (data.colors || []).join('|');
+  console.log('binning', dataArray.length);
+  dataArray.forEach(function(cData){
+    var colorKey = (cData.colors || []).join('|');
     byColors[colorKey] = byColors[colorKey] || [];
     byColors[colorKey].push(cData);
   });
   Object.keys(byColors).forEach(function(colorKey){
     var colorData = byColors[colorKey];
+    console.log('graphing', colorKey, colorData.length);
     colorData.forEach(function(cData1){
       cData1.neighbors = [];
       colorData.forEach(function(cData2){
-        if(MTG.get.cardDistance(cData1, cData2) === 1){
+        if(MTG.get.cardDistance(cData1, cData2) === 1 && cData1.cmc === cData2.cmc){
           cData1.neighbors.push(cData2);
         }
       });
@@ -131,16 +156,39 @@ MTG.drawGraph = function(data){
   });
 }
 MTG.filterData = function(data){
-  return data;
+  var dataArray = Object.values(data);
+  dataArray = dataArray.filter(function(cData){
+    var cmc = cData.cmc || 0;
+    return 3 <= cmc && cmc <= 4;
+  })
+  return dataArray;
 };
-MTG.addMetaData = function(data){
-  // MTG.drawGraph(data);
-  return data;
+MTG.filterDataArray = function(dataArray){
+  return dataArray.filter(function(cData){
+    return cData.neighbors.length > 0;
+  });
+};
+MTG.addMetaData = function(dataArray){
+  MTG.drawGraph(dataArray);
+  return dataArray;
 };
 MTG.rigData = function(data){
-  MTG.data = MTG.addMetaData(MTG.filterData(data));
-  return MTG.data;
+  MTG.rawData = data;
+  MTG.dataArray = MTG.filterDataArray(
+    MTG.addMetaData(
+      MTG.filterData(data)
+    )
+  );
+  return MTG.dataArray;
 };
+MTG.random = function(arr){
+  return arr[Math.floor(Math.random()*arr.length)];
+}
+MTG.randomPair = function(){
+  const cData1 = MTG.random(MTG.dataArray);
+  const cData2 = MTG.random(cData1.neighbors);
+  return [cData1, cData2];
+}
 MTG.init = function(){
   fetch('./AllCards.json')
     .then(function(response) {
@@ -150,9 +198,9 @@ MTG.init = function(){
       return response.json();
     })
     .then(function(data) {
-      data = MTG.rigData(data);
+      MTG.rigData(data);
       ReactDOM.render(
-        <Manager data={data} />,
+        <Manager />,
         document.getElementById('root')
       );
     });
