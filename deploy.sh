8040e44ffedf29f6e66ce153faccf1465e25226f
mpaulweeks <mpaulweeks@gmail.com>
2017-03-26 22:13:57 -0400

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,13 @@
+cd quiz-app
+
 npm run build
 mkdir ../docs
 rm -r ../docs
 mkdir ../docs
 cp -a build/ ../docs
 # echo 'mtg-quiz.mpaulweeks.com' > ../docs/CNAME
+
+cd ..
 git add .
 git commit -m 'deploy'
 git push
